e12383c4c000d7eac8ae521d2613a667677c843b
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2022-05-12 17:32:09 +0500

    marketplace first versin complete

--- src/views/TreasuryDashboard/TreasuryDashboard.tsx
+++ src/views/TreasuryDashboard/TreasuryDashboard.tsx
@@ -16,34 +16,43 @@ const TreasuryDashboard = memo(() => {
 
   const NFTCollection = async () => {
     const web3 = await ContractHelper(networkId);
-    const nft = new web3.eth.Contract(NFT_ABI, "0x6361910E7f9Ea0D5AF5cbecA3b3ba7D9C9FE57c9");
+    const nft = new web3.eth.Contract(NFT_ABI, "0xa758D9D3a37130A54e4c5F6757fbb87854240908");
     const tvl = await nft.methods;
     console.log(tvl);
   };
 
   const createAskOrder = async () => {
     const web3 = await ContractHelper(networkId);
-    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0x57CFa123fb1431A1C53A91AfEfCE510D731B909a");
+    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0xEE72D4baFfb8A86c2D7449b3e2b8d61d46264b35");
     const market = await MP.methods
-      .createAskOrder("0x6361910E7f9Ea0D5AF5cbecA3b3ba7D9C9FE57c9", 0, "2")
+      .createAskOrder("0xa758D9D3a37130A54e4c5F6757fbb87854240908", 1, "300")
+      .send({ from: address });
+    console.log({ market });
+  };
+
+  const modifyAskOrder = async () => {
+    const web3 = await ContractHelper(networkId);
+    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0xEE72D4baFfb8A86c2D7449b3e2b8d61d46264b35");
+    const market = await MP.methods
+      .modifyAskOrder("0xa758D9D3a37130A54e4c5F6757fbb87854240908", 1, "100")
       .send({ from: address });
     console.log({ market });
   };
 
   const buyNFT = async () => {
     const web3 = await ContractHelper(networkId);
-    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0x57CFa123fb1431A1C53A91AfEfCE510D731B909a");
+    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0xEE72D4baFfb8A86c2D7449b3e2b8d61d46264b35");
     const market = await MP.methods
-      .buyTokenUsingFTM("0x6361910E7f9Ea0D5AF5cbecA3b3ba7D9C9FE57c9", 0)
-      .send({ from: address, value: "2" });
+      .buyTokenUsingFTM("0xa758D9D3a37130A54e4c5F6757fbb87854240908", 1)
+      .send({ from: address, value: "300" });
     console.log({ market });
   };
 
   const ViewAskOrder = async () => {
     const web3 = await ContractHelper(networkId);
-    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0x57CFa123fb1431A1C53A91AfEfCE510D731B909a");
+    const MP = new web3.eth.Contract(MARKETPLACE_ABI, "0xEE72D4baFfb8A86c2D7449b3e2b8d61d46264b35");
     const orders = await MP.methods
-      .viewAsksByCollectionAndTokenIds("0x6361910E7f9Ea0D5AF5cbecA3b3ba7D9C9FE57c9", [0])
+      .viewAsksByCollectionAndTokenIds("0xa758D9D3a37130A54e4c5F6757fbb87854240908", [1])
       .call();
     console.log(orders);
   };
@@ -54,9 +63,17 @@ const TreasuryDashboard = memo(() => {
   });
   return (
     <div id="treasury-dashboard-view" className={`${isSmallScreen && "smaller"} ${isVerySmallScreen && "very-small"}`}>
-      <button onClick={createAskOrder}>Create Ask Order</button>
+      <button onClick={createAskOrder}>Create Ask Order</button> <br />
+      <br />
       <button onClick={ViewAskOrder}>View Ask order</button>
+      <br />
+      <br />
       <button onClick={buyNFT}>BUY NFT</button>
+      <br />
+      <br />
+      <button onClick={buyNFT}>Modify Ask Order</button>
+      <br />
+      <br />
     </div>
   );
 });
